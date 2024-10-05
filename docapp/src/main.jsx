68109c4bc054ba5dc19985473dca2640d9ecdd13
MFahid <fahidali333123@gmail.com>
2024-10-05 16:41:26 +0500

    add only  pages files in page folder

--- docapp/src/main.jsx
+++ docapp/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom' 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
